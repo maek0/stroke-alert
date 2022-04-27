2df64533c03e30ee3715712cf084e7fae091aa0d
maek0 <kaitlinkramer014@gmail.com>
2022-04-27 11:52:28 -0500

    updated for running without the GUI

--- run.py
+++ run.py
@@ -2,16 +2,15 @@ from showVidFcns import strokedet
 from showVidFcns import strokedetII
 from showVidFcns import setbase
 from showVidFcns import handCalc
+# from strokedet_allfun import strokedet
+# from strokedet_allfun import strokedetII
+# from strokedet_allfun import setbase
+# from strokedet_allfun import handCalc
 
 # setbase()
 
-# result = 0 --> likely no stroke
-# result = 1 --> likely stroke
-
-# r is the average of p-values of determining characteristics
-
 r = strokedet()
-if r < 0.5:
+if r < 0.65:
     print("--------------------")
     print("\nRaise your Hands!\n")
     print("--------------------")
@@ -21,17 +20,17 @@ else:
 
 if ruling == 0:
     opr = 100*(1-r)
-    print("--------------------\n")
+    print("\n--------------------\n")
     print("No Stroke Detected\n")
-    if r < 0.5:
-        print("There is some uncertainty in the probability. For more accuracy, move to a place with better lighting.")
-    print("There is a %.2f percent chance you are experiencing a stroke\n" % opr)
+    if r < 0.65:
+        print("There is some uncertainty in the probability. For more accuracy, move to a place with better lighting.\nIf you feel dizzy or numbness in one arm or one side of your face, call EMS.")
+    print("\nThere is a %.2f percent chance you are experiencing a stroke\n" % opr)
     print("--------------------")
 else:
     opr = 100*(1-r)
     print("--------------------\n")
-    print("Stroke Likely: call EMS\n")
-    print("\nThere is a %.2f percent chance you are experiencing a stroke" % opr)
+    print("Stroke Likely: call EMS immediately!")
+    print("\nThere is a %.2f percent chance you are experiencing a stroke\n" % opr)
     print("--------------------")
 
 # print(ruling)
