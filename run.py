6a57ceb7d6325372bf9d99a097a5f3bcd6d27667
maek0 <kaitlinkramer014@gmail.com>
2022-04-27 11:35:36 -0500

    thresholds

--- run.py
+++ run.py
@@ -1,21 +1,16 @@
-# from showVidFcns import strokedet
-# from showVidFcns import strokedetII
-# from showVidFcns import setbase
-# from showVidFcns import handCalc
-from strokedet_allfun import strokedet
-from strokedet_allfun import strokedetII
-from strokedet_allfun import setbase
-from strokedet_allfun import handCalc
+from showVidFcns import strokedet
+from showVidFcns import strokedetII
+from showVidFcns import setbase
+from showVidFcns import handCalc
+# from strokedet_allfun import strokedet
+# from strokedet_allfun import strokedetII
+# from strokedet_allfun import setbase
+# from strokedet_allfun import handCalc
 
 # setbase()
 
-# result = 0 --> likely no stroke
-# result = 1 --> likely stroke
-
-# r is the average of p-values of determining characteristics
-
 r = strokedet()
-if r < 0.35:
+if r < 0.65:
     print("--------------------")
     print("\nRaise your Hands!\n")
     print("--------------------")
@@ -27,7 +22,7 @@ if ruling == 0:
     opr = 100*(1-r)
     print("\n--------------------\n")
     print("No Stroke Detected\n")
-    if r < 0.35:
+    if r < 0.65:
         print("There is some uncertainty in the probability. For more accuracy, move to a place with better lighting.\nIf you feel dizzy or numbness in one arm or one side of your face, call EMS.")
     print("\nThere is a %.2f percent chance you are experiencing a stroke\n" % opr)
     print("--------------------")
