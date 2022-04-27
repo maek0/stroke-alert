7361ffca3442ed524b1b6d6da1fa1662bb6f2033
maek0 <kaitlinkramer014@gmail.com>
2022-04-27 11:27:41 -0500

    hand drawing style

--- showVidFcns.py
+++ showVidFcns.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import itertools
 import time
 from shapely.geometry import Polygon
 import math
@@ -24,11 +25,11 @@ def facepose(sec):
     HandY = []
 
     drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     endtime = time.time() + sec
 
-    face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.8, min_tracking_confidence=0.7)
-    hands = mp_hands.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    face_mesh = mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.8, min_tracking_confidence=0.8)
+    hands = mp_hands.Hands(model_complexity=0, min_detection_confidence=0.8, min_tracking_confidence=0.8)
 
     while cap.isOpened():
         success, image = cap.read()
@@ -71,9 +72,7 @@ def facepose(sec):
                 mp_drawing.draw_landmarks(
                     image,
                     hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
+                    mp_hands.HAND_CONNECTIONS,)
 
         cv2.imshow('MediaPipe Face Mesh',cv2.flip(image, 1))
 
@@ -90,6 +89,7 @@ def facepose(sec):
 def calcVars(sec):
     
     FaceX, FaceY = facepose(sec)
+    mp_face_mesh = mp.solutions.face_mesh
 
     FaceXarray = np.array(FaceX[0:477])
     del FaceX[0:477]
@@ -106,8 +106,10 @@ def calcVars(sec):
     midInd = [10,9,8,168,6,197,195,5,4,1,19,94,2,164,0,17,18,200,199]
     rmouthInd = [76,184,40,39,37,0,17,84,181,91,146]
     lmouthInd = [0,267,269,270,408,306,307,321,405,314,17]
-    leInd = [362,398,384,385,386,387,388,466,263,249,390,373,374,380,381,382]
-    reInd = [33,246,161,160,159,158,157,173,133,155,154,153,145,144,163,7]
+    leInd = list(set(itertools.chain(*mp_face_mesh.FACEMESH_LEFT_EYE)))
+    reInd = list(set(itertools.chain(*mp_face_mesh.FACEMESH_RIGHT_EYE)))
+    # leInd = [362,398,384,385,386,387,388,466,263,249,390,373,374,380,381,382]
+    # reInd = [33,246,161,160,159,158,157,173,133,155,154,153,145,144,163,7]
 
     midXshift = np.empty((FaceXarray.shape[0],0),dtype=np.float64)
     lmouthA = np.empty((FaceXarray.shape[0],0),dtype=np.float64)
@@ -197,7 +199,7 @@ def calcVars(sec):
     return saveArray
 
 def setbase():
-    baseArray = calcVars(30)
+    baseArray = calcVars(10)
     savebase(baseArray)
 
 def savebase(baseArray):
@@ -230,7 +232,7 @@ def handdet(sec):
     Hand2Y = []
 
     drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     endtime = time.time() + sec
 
     hands = mp_hands.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5)
@@ -249,14 +251,11 @@ def handdet(sec):
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
         if hands_result.multi_hand_landmarks:
-            
             for hand_landmarks in hands_result.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
                     image,
                     hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
+                    mp_hands.HAND_CONNECTIONS,)
 
             for hand_no, hand_lm in enumerate(hands_result.multi_hand_landmarks):
                 if hand_no == 0:
@@ -337,12 +336,12 @@ def handCalc(sec):
         else:
             sz = Hand1Yarray.shape[0]
 
-        eqH1 = Hand1Yarray[0:sz,0]
-        eqH2 = Hand2Yarray[0:sz,0]
-        relDiff = [a/b for a,b in zip(eqH1,eqH2)]
-        wristDiff = np.abs(eqH1-eqH2)
+    eqH1 = Hand1Yarray[0:sz]
+    eqH2 = Hand2Yarray[0:sz]
+    relDiff = [a/b for a,b in zip(eqH1,eqH2)]
+    wristDiff = np.abs(eqH1-eqH2)
         
-    return eqH1, eqH2, wristDiff, relDiff
+    return wristDiff, relDiff
 
 def strokedet():
     past = []
@@ -353,8 +352,8 @@ def strokedet():
     isExist = os.path.exists(arrpath)
 
     if not isExist:
-        # raise ValueError("Need to establish baseline images before running!")
-        Warning("Need to establish baseline images before running!")
+        raise ValueError("Need to establish baseline images before running!")
+        # Warning("Need to establish baseline images before running!")
     else:
         if glob.glob(framefind):
             for file in glob.glob(framefind):
@@ -371,8 +370,8 @@ def strokedet():
                 else:
                     past = np.hstack((past, temp))
         else:
-            # raise ValueError("Need to establish baseline images before running!")
-            Warning("Need to establish baseline images before running!")
+            raise ValueError("Need to establish baseline images before running!")
+            # Warning("Need to establish baseline images before running!")
 
     # array order:
     # [[allMidShift], 
@@ -387,7 +386,7 @@ def strokedet():
     # [EyeRatioHeight], 
     # [MouthCorners]]
 
-    array = calcVars(8)
+    array = calcVars(5)
     _, p1 = stats.ttest_ind(past[3],array[3])       # EyeRatioArea p-value
     _, p3 = stats.ttest_ind(past[9],array[9])       # EyeRatioHeight p-value
     _, p4 = stats.ttest_ind(past[10],array[10])     # MouthCorners p-value
@@ -396,7 +395,7 @@ def strokedet():
 
     r = np.mean(ps)
 
-    if r >= 0.6:
+    if r >= 0.65:
         dir_path = os.getcwd()
         opsys = platform.system()
         
@@ -417,12 +416,12 @@ def strokedet():
     return r
 
 def strokedetII(r):
-    _, _, wristDiff, relDiff = handCalc(8)
-    relDiff = np.mean(relDiff[3:])
+    wristDiff, rDiff = handCalc(5)
+    rDiff = np.mean(rDiff[3:])
     
-    if relDiff >= 2:
+    if rDiff >= 1.5:
         ruling = 1
-    elif relDiff <= 0.5:
+    elif rDiff <= 0.7:
         ruling = 1
     else:
         ruling = 0
